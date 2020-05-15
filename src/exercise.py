fb2605974c4945f23cbc2ecc71f34889b57e4f89
cabosh <callum.bosher@gmail.com>
2020-05-15 15:52:46 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number: "))
+
+    add = num1 + num2
+    subtract = num1 - num2
+    multiply = num1 * num2
+    divide = num1 / num2
+
+    print(str(num1) + " + " + str(num2) + " = " + str(add))
+    print(str(num1) + " - " + str(num2) + " = " + str(subtract))
+    print(str(num1) + " * " + str(num2) + " = " + str(multiply))
+    print(str(num1) + " / " + str(num2) + " = " + str(divide))
 
 if __name__ == '__main__':
     main()
